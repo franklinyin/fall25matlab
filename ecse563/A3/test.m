95fd814481b122cd2bd20e6c73ca3858eb762d2b
Frank Yin <achieveryin@gmail.com>
2025-11-17 18:52:35 -0500

    a3 part 1c added

---
+++ ecse563/A3/test.m
@@ -0,0 +1,27 @@
+%% Q1
+mdl = 'part1cde';
+load_system(mdl);
+set_param(mdl, 'StopTime', '30');
+
+%% Run simulation
+out = sim(mdl);
+
+%% q1c
+
+fromSim = out.get('omegaPlot');
+if isempty(fromSim)
+    fromSim = out.logsout.get('wScope');
+end
+
+figure;
+hold on; grid on;
+for i = 1:fromSim.numElements
+    toPlot = fromSim.get(i);
+    plot(toPlot.Values.Time, toPlot.Values.Data, 'DisplayName', toPlot.Name);
+end
+xlabel('Time [s]'); ylabel('\Delta f [Hz]');
+title('Part 1: Frequency deviation'); saveas(gcf, 'part1_freq.png');
+legend('show');
+
+
+
