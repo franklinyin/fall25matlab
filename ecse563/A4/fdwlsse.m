d610a97b4116bdc5174a37d335b0533b35590fac
Frank Yin <achieveryin@gmail.com>
2025-12-16 20:56:50 -0500

    simplied Q4

--- ecse563/A4/fdwlsse.m
+++ ecse563/A4/fdwlsse.m
@@ -42,80 +42,51 @@ for l = 1:nline
     Ybus(k,i) = Ybus(k,i) - y;
 end
 
-% Stack measurements
+% Extract measurements and build weight matrix
 z = [];
-bus1 = [];
-bus2 = [];
-mtype = [];
 sigma2 = [];
 
-% P injections
 if ~isempty(Pinj)
-    z = [z ; Pinj(:,2)];
-    bus1 = [bus1 ; Pinj(:,1)];
-    bus2 = [bus2 ; zeros(size(Pinj,1),1)];
-    mtype = [mtype; 1*ones(size(Pinj,1),1)];
-    sigma2 = [sigma2 ; Pinj(:,3).^2];
+    z = [z; Pinj(:,2)];
+    sigma2 = [sigma2; Pinj(:,3).^2];
 end
 
-% Q injections
 if ~isempty(Qinj)
-    z = [z ; Qinj(:,2)];
-    bus1 = [bus1 ; Qinj(:,1)];
-    bus2 = [bus2 ; zeros(size(Qinj,1),1)];
-    mtype = [mtype; 2*ones(size(Qinj,1),1)];
-    sigma2 = [sigma2 ; Qinj(:,3).^2];
+    z = [z; Qinj(:,2)];
+    sigma2 = [sigma2; Qinj(:,3).^2];
 end
 
-% P flows
 if ~isempty(Pflow)
-    z = [z ; Pflow(:,3)];
-    bus1 = [bus1 ; Pflow(:,1)];
-    bus2 = [bus2 ; Pflow(:,2)];
-    mtype = [mtype; 3*ones(size(Pflow,1),1)];
-    sigma2 = [sigma2 ; Pflow(:,4).^2];
+    z = [z; Pflow(:,3)];
+    sigma2 = [sigma2; Pflow(:,4).^2];
 end
 
-% Q flows
 if ~isempty(Qflow)
-    z = [z ; Qflow(:,3)];
-    bus1 = [bus1 ; Qflow(:,1)];
-    bus2 = [bus2 ; Qflow(:,2)];
-    mtype = [mtype; 4*ones(size(Qflow,1),1)];
-    sigma2 = [sigma2 ; Qflow(:,4).^2];
+    z = [z; Qflow(:,3)];
+    sigma2 = [sigma2; Qflow(:,4).^2];
 end
 
-% V magnitudes
 if ~isempty(Vnode)
-    z = [z ; Vnode(:,2)];
-    bus1 = [bus1 ; Vnode(:,1)];
-    bus2 = [bus2 ; zeros(size(Vnode,1),1)];
-    mtype = [mtype; 5*ones(size(Vnode,1),1)];
-    sigma2 = [sigma2 ; Vnode(:,3).^2];
+    z = [z; Vnode(:,2)];
+    sigma2 = [sigma2; Vnode(:,3).^2];
 end
 
-m = numel(z);
 W = diag(1 ./ sigma2);
 
 % Initial state guess
 delta_state = zeros(nbus-1,1);
 V = ones(nbus,1);
 x = [delta_state; V];
-nstate = numel(x);
-
-% Function handle for measurement model
-measfun = @(xx) measurement_model(xx, Ybus, nbus, ...
-                                  nfrom, nto, yser, bsh, ...
-                                  mtype, bus1, bus2);
 
 % Iterative WLS
 tic;
 for k = 1:maxiter
-    h = measfun(x);
+    h = measurement_model(x, Ybus, nbus, nfrom, nto, yser, bsh, ...
+                         Pinj, Qinj, Pflow, Qflow, Vnode);
     r = z - h;
     
-    % Numerical Jacobian
-    H = numerical_jacobian(measfun, x, h);
+    H = numerical_jacobian(x, h, Ybus, nbus, nfrom, nto, yser, bsh, ...
+                          Pinj, Qinj, Pflow, Qflow, Vnode);
     
     G = H.' * (W * H);
     rhs = H.' * (W * r);
@@ -137,69 +108,92 @@ V = x(nbus:end);
 end
 
 % =====================================================================
-function h = measurement_model(x, Ybus, nbus, ...
-                               nfrom, nto, yser, bsh, ...
-                               mtype, bus1, bus2)
-% Build predicted measurements h(x)
+function h = measurement_model(x, Ybus, nbus, nfrom, nto, yser, bsh, ...
+                                Pinj, Qinj, Pflow, Qflow, Vnode)
+
 delta_state = x(1:nbus-1);
 V = x(nbus:end);
 
 Va = [0; delta_state];
 Vcmp = V .* exp(1j*Va);
 
-% Bus injections
 Iinj = Ybus * Vcmp;
 Sbus = Vcmp .* conj(Iinj);
 Pbus = real(Sbus);
 Qbus = imag(Sbus);
 
-h = zeros(numel(mtype),1);
+h = [];
 
-for k = 1:numel(mtype)
-    t = mtype(k);
-    i = bus1(k);
-    j = bus2(k);
-    
-    switch t
-        case 1      % Pinj at bus i
-            h(k) = Pbus(i);
-        case 2      % Qinj at bus i
-            h(k) = Qbus(i);
-        case 3      % Pflow from i to j
-            idx = find(nfrom==i & nto==j, 1);
-            if isempty(idx)
-                idx = find(nfrom==j & nto==i, 1);
-                i2 = j; j = i;
-            else
-                i2 = i;
-            end
-            y = yser(idx);
-            bs = bsh(idx);
-            Iij = (Vcmp(i2) - Vcmp(j)) * y + Vcmp(i2) * bs;
-            Sij = Vcmp(i2) * conj(Iij);
-            h(k) = real(Sij);
-        case 4      % Qflow from i to j
-            idx = find(nfrom==i & nto==j, 1);
-            if isempty(idx)
-                idx = find(nfrom==j & nto==i, 1);
-                i2 = j; j = i;
-            else
-                i2 = i;
-            end
-            y = yser(idx);
-            bs = bsh(idx);
-            Iij = (Vcmp(i2) - Vcmp(j)) * y + Vcmp(i2) * bs;
-            Sij = Vcmp(i2) * conj(Iij);
-            h(k) = imag(Sij);
-        case 5      % V magnitude at bus i
-            h(k) = abs(Vcmp(i));
+% P injections
+if ~isempty(Pinj)
+    for k = 1:size(Pinj,1)
+        i = Pinj(k,1);
+        h = [h; Pbus(i)];
     end
 end
+
+% Q injections
+if ~isempty(Qinj)
+    for k = 1:size(Qinj,1)
+        i = Qinj(k,1);
+        h = [h; Qbus(i)];
+    end
+end
+
+% P flows
+if ~isempty(Pflow)
+    for k = 1:size(Pflow,1)
+        i = Pflow(k,1);
+        j = Pflow(k,2);
+        idx = find(nfrom==i & nto==j, 1);
+        if isempty(idx)
+            idx = find(nfrom==j & nto==i, 1);
+            i2 = j; j2 = i;
+        else
+            i2 = i; j2 = j;
+        end
+        y = yser(idx);
+        bs = bsh(idx);
+        Iij = (Vcmp(i2) - Vcmp(j2)) * y + Vcmp(i2) * bs;
+        Sij = Vcmp(i2) * conj(Iij);
+        h = [h; real(Sij)];
+    end
+end
+
+% Q flows
+if ~isempty(Qflow)
+    for k = 1:size(Qflow,1)
+        i = Qflow(k,1);
+        j = Qflow(k,2);
+        idx = find(nfrom==i & nto==j, 1);
+        if isempty(idx)
+            idx = find(nfrom==j & nto==i, 1);
+            i2 = j; j2 = i;
+        else
+            i2 = i; j2 = j;
+        end
+        y = yser(idx);
+        bs = bsh(idx);
+        Iij = (Vcmp(i2) - Vcmp(j2)) * y + Vcmp(i2) * bs;
+        Sij = Vcmp(i2) * conj(Iij);
+        h = [h; imag(Sij)];
+    end
+end
+
+% V magnitudes
+if ~isempty(Vnode)
+    for k = 1:size(Vnode,1)
+        i = Vnode(k,1);
+        h = [h; abs(Vcmp(i))];
+    end
+end
+
 end
 
 % =====================================================================
-function H = numerical_jacobian(measfun, x, h0)
-% Simple finite-difference Jacobian
+function H = numerical_jacobian(x, h0, Ybus, nbus, nfrom, nto, yser, bsh, ...
+                                 Pinj, Qinj, Pflow, Qflow, Vnode)
+
 nstate = numel(x);
 m = numel(h0);
 H = zeros(m, nstate);
@@ -208,7 +202,8 @@ eps_fd = 1e-6;
 for j = 1:nstate
     xpert = x;
     xpert(j) = xpert(j) + eps_fd;
-    hpert = measfun(xpert);
+    hpert = measurement_model(xpert, Ybus, nbus, nfrom, nto, yser, bsh, ...
+                              Pinj, Qinj, Pflow, Qflow, Vnode);
     H(:,j) = (hpert - h0) / eps_fd;
 end
 end
